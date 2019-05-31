3cd40cf529b6c38507bbefb9fc88fcee11743e58
daliasen <18193042+daliasen@users.noreply.github.com>
2019-05-31 19:10:37 +0100

    replace the API key from the command line arguments with the global variable from the config file

--- metOffice/temperatureUK.py
+++ metOffice/temperatureUK.py
@@ -18,7 +18,7 @@ def request_met_office(resource, params = ''):
   response.raise_for_status()
   return response.json()
 
-def get_location_id(api_key, desired_location = 'London'):
+def get_location_id(desired_location = 'London'):
   locations = request_met_office('val/wxfcs/all/json/sitelist') # daily and three-hourly forecast
   locations = locations['Locations']['Location']
 
@@ -30,44 +30,33 @@ def get_location_id(api_key, desired_location = 'London'):
     sys.exit('location ID not found')
   return location_id, location_name
   
-def get_closest_timestamp(api_key):
+def get_closest_timestamp():
   # find the timestamp closest to the current time
   capabilities = request_met_office('val/wxfcs/all/json/capabilities', params = 'res=3hourly')
   timestamps = capabilities['Resource']['TimeSteps']['TS'] # a list of timestamps
-  current_timestamp = datetime.today()
+  current_timestamp = datetime.now()
 
   deltas = []
   for timestamp in timestamps:
     timestamp_datetime = datetime.strptime(timestamp, TIME_FORMAT)
     delta = current_timestamp - timestamp_datetime
     deltas.append(abs(delta.total_seconds()))
-    #print('timestamp: ' + str(timestamp))
-    #print('delta: ' + str(delta))
-    #print('delta, seconds: ' + str(delta.total_seconds()))
-    #print('delta, seconds type: ' + str(type(delta.total_seconds())))
       
   closest_stamp = timestamps[deltas.index(min(deltas))]
-  #print('deltas min index: ' + str(deltas.index(min(deltas))))
   print('Closest timestamp: ' + str(closest_stamp))
   
   return closest_stamp
 
-# Get the API key from the command line arguments
-if len(sys.argv) < 2:
-  print('Usage: temperatureUK.py api_key\n Optional: location (London by default)')
-  sys.exit()
-  
-api_key = sys.argv[1]
-
-if len(sys.argv) > 2:
-  desired_location = ' '.join(sys.argv[2:])
+# Get the location from the command line arguments if specified
+if len(sys.argv) > 1:
+  desired_location = ' '.join(sys.argv[1:])
 
 if 'desired_location' in locals():
-  location_id, location_name = get_location_id(api_key, desired_location)
+  location_id, location_name = get_location_id(desired_location)
 else:
-  location_id, location_name = get_location_id(api_key) # use default location
+  location_id, location_name = get_location_id() # use default location
 
-current_timestamp = get_closest_timestamp(api_key)
+current_timestamp = get_closest_timestamp()
 
 # Download the JSON data from metoffice.gov.uk API.
 weather = request_met_office('val/wxfcs/all/json/' + location_id, params = 'res=3hourly&time=' + current_timestamp) # JSON to Python dict
